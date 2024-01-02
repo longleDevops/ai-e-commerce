14da754295afa9defa828e0c3cb54a7cae44123b
long <137044122+longleDevops@users.noreply.github.com>
2024-01-01 21:32:53 -0800

    Create a complete form

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,16 +1,23 @@
-import { Button } from '@/components/ui/button'
-import { UserButton } from '@clerk/nextjs'
-import Image from 'next/image'
 "use client"
 
+import {useEffect} from "react"
+
 import {Modal} from "@/components/ui/modal"
+import { useStoreModal } from '@/hooks/use-store-modal'
 
 export default function Home() {
+  const onOpen = useStoreModal((state)=>state.onOpen);
+  const isOpen = useStoreModal((state)=> state.isOpen)
+
+  useEffect(()=>{
+    if (!isOpen) {
+      onOpen();
+    }
+  }, [isOpen,onOpen])
+
   return (
     <div className="p-4">
-      <Modal title="Test" description="Test desc" isOpen onClose={()=>{}}>
-        Children
-      </Modal>
+      Root page
     </div>
   )
 }
