4c69106973ccc0e38a23297ad5b20e35d58b559b
long <137044122+longleDevops@users.noreply.github.com>
2024-01-03 23:26:33 -0800

    Complete store combo box

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,13 +1,28 @@
-import { UserButton } from "@clerk/nextjs"
+import { UserButton, auth } from "@clerk/nextjs"
+import { redirect } from "next/navigation"
 
 import { MainNav } from "@/components/main-nav"
 import StoreSwitcher from "./ui/store-switcher"
+import prismadb from "@/lib/prismadb"
 
-const navbar = () => {
+const navbar = async () => {
+  const { userId } = auth()
+
+  if (!userId) {
+    redirect("/sign-in")
+  }
+
+  // return an array of stores with findMany method
+  const stores = await prismadb.store.findMany({
+    where: {
+      userId
+    }
+  })
+
   return (
     <div className="border-b">
       <div className="flex items-center p-4 h-16">
-        <StoreSwitcher/>
+        <StoreSwitcher items={stores}/>
         <MainNav className="mx-6"/>
         {/* ml-auto will push everything to the right */}
         <div className="ml-auto flex items-center space-x-4">
